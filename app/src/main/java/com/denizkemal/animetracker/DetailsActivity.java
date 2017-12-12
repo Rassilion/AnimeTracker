a1ca37585355bde891e85b526e7650682fb00d3d
kasabali1122 <piratekemal@gmail.com>
2017-12-13 00:11:27 +0300

    Detail page

--- app/src/main/java/com/denizkemal/animetracker/DetailsActivity.java
+++ app/src/main/java/com/denizkemal/animetracker/DetailsActivity.java
@@ -18,12 +18,21 @@ import com.denizkemal.animetracker.api.BaseModels.AnimeManga.Manga;
 import com.denizkemal.animetracker.api.MALApi;
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Text;
+
 
 public class DetailsActivity extends AppCompatActivity implements NetworkTask.NetworkTaskListener {
 
     public TextView nameLabel;
     public ImageView image;
     public TextView Synopsis;
+    public TextView episodes;
+    public TextView rankPoint;
+    public TextView avaregePoint;
+    public TextView avarageCount;
+    public TextView typeOf;
+    public TextView openingTheme;
+    public TextView statusLabel;
     public Anime currentAnime;
     public Manga currentManga;
     @Override
@@ -60,8 +69,7 @@ public class DetailsActivity extends AppCompatActivity implements NetworkTask.Ne
                     .load(currentAnime.getImageUrl())
                     .error(R.drawable.ic_naruto)
                     .into(image);
-            Synopsis = (TextView)findViewById(R.id.synopsis);
-            Synopsis.setText(currentAnime.getSynopsisString());
+
         }
         else if (type.equals("manga")){
             currentManga =  MangaFragment.mangaList.get(a);
@@ -87,10 +95,63 @@ public class DetailsActivity extends AppCompatActivity implements NetworkTask.Ne
             currentAnime = (Anime) result;
             Synopsis = (TextView) findViewById(R.id.synopsis);
             Synopsis.setText(Html.fromHtml(currentAnime.getSynopsisString()));
+
+
+            episodes = (TextView) findViewById(R.id.episodes);
+            episodes.setText(Integer.toString(currentAnime.getEpisodes()));
+
+            rankPoint = (TextView) findViewById(R.id.rankPoint);
+            rankPoint.setText(Integer.toString(currentAnime.getRank()));
+
+            avaregePoint =(TextView) findViewById(R.id.avaragePoint);
+            avaregePoint.setText(currentAnime.getAverageScore() + " / 10");
+
+            avarageCount = (TextView) findViewById(R.id.avarageCount);
+            avarageCount.setText(currentAnime.getAverageScoreCount());
+
+            typeOf = (TextView) findViewById(R.id.typeLabel);
+            typeOf.setText(currentAnime.getType());
+
+            String openingMusic="";
+            for (String items:currentAnime.getOpeningTheme()) {
+                openingMusic +=(items + "\n");
+            }
+            openingTheme = (TextView) findViewById(R.id.openingLabel);
+            openingTheme.setText(openingMusic);
+
+            statusLabel = (TextView)findViewById(R.id.statusLabel);
+            statusLabel.setText(currentAnime.getStatus());
+
+
         }else{
             currentManga = (Manga) result;
             Synopsis = (TextView) findViewById(R.id.synopsis);
             Synopsis.setText(Html.fromHtml(currentManga.getSynopsisString()));
+
+            episodes = (TextView) findViewById(R.id.episodes);
+            episodes.setText(Integer.toString(currentManga.getChapters()));
+
+            rankPoint = (TextView) findViewById(R.id.rankPoint);
+            rankPoint.setText(Integer.toString(currentManga.getRank()));
+
+            avaregePoint =(TextView) findViewById(R.id.avaragePoint);
+            avaregePoint.setText(currentManga.getAverageScore() + " / 10");
+
+            avarageCount = (TextView) findViewById(R.id.avarageCount);
+            avarageCount.setText(currentManga.getAverageScoreCount());
+
+            typeOf = (TextView) findViewById(R.id.typeLabel);
+            typeOf.setText(currentManga.getType());
+        /*
+            String openingMusic="";
+            for (String items:currentManga.getOpeningTheme()) {
+                openingMusic +=(items + "\n");
+            }
+            openingTheme = (TextView) findViewById(R.id.openingLabel);
+            openingTheme.setText(openingMusic);*/
+
+            statusLabel = (TextView)findViewById(R.id.statusLabel);
+            statusLabel.setText(currentManga.getStatus());
         }
     }
 
